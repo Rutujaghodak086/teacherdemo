03a18403d15c2c93ba0815b5c9660c90298e7495
Rutuja ghodake <ghodakerutuja086@gmail.com>
2025-12-08 15:39:03 +0530

    teacher project

---
+++ Teacher1/src/main/java/com/example/demo/Teacher1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teacher1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teacher1Application.class, args);
+	}
+
+}
